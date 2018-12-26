a8fee71dd9ae3be29e0f137b264442368a378d48
Jens Schwarzer <jens.schwarzer@marchv.com>
2018-12-26 18:22:28 +0100

    Clean a bit and add skeleton for ifu download

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,19 +2,40 @@ import Vapor
 
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
-    // Basic "It works" example
-    router.get { req in
-        return "It works!"
-    }
-    
-    // Basic "Hello, world!" example
-    router.get("hello") { req in
-        return "Hello, world!"
-    }
 
-    // Example of configuring a controller
-    let todoController = TodoController()
-    router.get("todos", use: todoController.index)
-    router.post("todos", use: todoController.create)
-    router.delete("todos", Todo.parameter, use: todoController.delete)
+    router.post(IfuRequest.self, at: "ifu") { req, ifuRequest -> IfuResponse in
+        
+        guard let appVariant = AppVariant(rawValue: ifuRequest.appVariant) else {
+            throw Abort(.badRequest, reason: "Unknown appVariant (\(ifuRequest.appVariant)).")
+        }
+        
+        guard let xblehiid = Data(base64Encoded: ifuRequest.xblehiid) else {
+            throw Abort(.badRequest, reason: "Failed to decode base64 payload (xblehiid).")
+        }
+        
+        return IfuResponse(path: "BIMS: \(xblehiid) \(appVariant) \(ifuRequest.modelName) languageCode: \(ifuRequest.languageCode); regionCode: \(ifuRequest.regionCode)")
+    }
+}
+
+enum AppVariant: String {
+    case oticon
+    case bernafon
+    case sonic
+    case kind
+    case oem
+    case philips
+}
+
+struct IfuRequest: Content {
+    let appVariant: String   // oticon, bernafon, sonic, kind, oem, philips
+    
+    let modelName: String    // Device Information Service Model Number String
+    let xblehiid: String     // Base64 encoded
+    
+    let languageCode: String // ISO 639-1
+    let regionCode: String   // ISO 3166-2
+}
+
+struct IfuResponse: Content {
+    let path: String
 }
